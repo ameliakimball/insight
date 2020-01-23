46833aa075650f3817257ee7708f90cc8fde96e8
your_username <akimbal2@illinois.edu>
2020-01-23 10:33:40 -0800

    adds prediction functionality in get_input_doglist. not modular,badcode

--- get_input_doglist.py
+++ get_input_doglist.py
@@ -4,6 +4,7 @@ import os
 import requests
 import json
 import pandas as pd
+import pickle
 
 KEY = os.environ['PETFINDER_KEY']
 SECRET = os.environ['PETFINDER_SECRET']
@@ -35,24 +36,39 @@ status = 'adoptable'
 respdf = pd.DataFrame()
 
 req_url = f'https://api.petfinder.com/v2/animals?type={type}&limit={limit}&page={page}&status={status}&organization={organization}'
-response = requests.get(req_url, headers= new_header)
+response = requests.get(req_url, headers = new_header)
 print(response)
 my_dict =json.loads(response.text)
 animals = my_dict.get('animals')
 df = pd.io.json.json_normalize(animals)
 respdf = respdf.append(df)
 
+respdf = respdf[['age','gender','size']]
+respdf['City'] = 'Chicago'
+cols_to_transform = ['age', 'gender', 'size','City']
+#add encoding that matches model
 
-#clean and preprocess the input data 
+preddat = pd.get_dummies(respdf, columns = cols_to_transform )
 
+#fill in dummy coded columns if there are missing values 
+preddat['City_StLouis'] = 0
+preddat['City_Indy'] = 0
+preddat['age_young'] = 0
+preddat['size_Extra Large'] = 0
+
+
+print(preddat.head())
 
 #load model and make predictions 
-
 loaded_model = pickle.load(open('draft_pet_model.sav', 'rb'))
 
+pred_targ_hours = loaded_model.predict(preddat)
 
+preddat['pred_targ_hours'] = pred_targ_hours.tolist()
 
+#remember that the returned hours are normalized and log transformed!
 
+print(preddat.head())
 
 
 
