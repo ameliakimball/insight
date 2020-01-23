e111f6fe1a4af9e859f5b4927c59db924167fd1e
your_username <akimbal2@illinois.edu>
2020-01-23 14:13:50 -0800

    updates html templates for my app

--- flask/flaskexample/views.py
+++ flask/flaskexample/views.py
@@ -1,37 +1,16 @@
-from a_Model import ModelIt
+
 from flask import render_template
 from flask import request 
 from flaskexample import app
-from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
-import psycopg2
 
-user = 'post-doc' #add your username here (same as previous postgreSQL)            
-host = 'localhost'
-dbname = 'birth_db'
-db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-con = None
-con = psycopg2.connect(database = dbname, user = user)
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html",
-       title = 'Home', user = { 'nickname': 'Miguel' },
+       title = 'Home', user = { 'nickname': 'Dog lover, go to the input page to test the app' },
        )
-
-@app.route('/db')
-def birth_page():
-    sql_query = """                                                                       
-                SELECT * FROM birth_data_table WHERE delivery_method='Cesarean';          
-                """
-    query_results = pd.read_sql_query(sql_query,con)
-    births = ""
-    for i in range(0,10):
-        births += query_results.iloc[i]['birth_month']
-        births += "<br>"
-    return births
 
 @app.route('/input')
 def cesareans_input():
@@ -39,15 +18,7 @@ def cesareans_input():
 
 @app.route('/output')
 def cesareans_output():
-  #pull 'birth_month' from input field and store it
-  patient = request.args.get('birth_month')
-    #just select the Cesareans  from the birth dtabase for the month that the user inputs
-  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
-  print(query)
-  query_results=pd.read_sql_query(query,con)
-  print(query_results)
-  births = []
-  for i in range(0,query_results.shape[0]):
-      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-      the_result = ModelIt(patient,births)
-      return render_template("output.html", births = births, the_result = the_result)
+  #pull 'shelter_id' from input field and store it
+  #patient = request.args.get('shelter_id')
+  mylist_of_dicts = [{'amelia':'The best', 'dogs':'alsogreat','cats':'less so'},{'amelia':'notsogreat', 'dogs':'alsogreat','cats':'less so'}]
+  return render_template("output.html", mylist_of_dicts = mylist_of_dicts)
